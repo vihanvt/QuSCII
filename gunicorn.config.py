b3d40b58151ed1ba7e6187eb267d42ae88f3021b
vihanvt <vihan.vnt@gmail.com>
2025-04-18 22:08:19 +0530

    I HATE THIS

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -1,4 +1,4 @@
 # gunicorn.config.py
-timeout = 120  # increase this from default 30s to 120s
+timeout = 600  # increase this from default 30s to 120s
 workers = 1    # can increase if needed
 threads = 2    # optional, improves concurrency
